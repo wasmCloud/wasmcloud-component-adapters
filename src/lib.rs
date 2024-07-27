1fe9189498d274e37ea6551a3fd7d2e65df54fae
Roman Volosatovs <rvolosatovs@riseup.net>
2024-07-27 15:42:29 +0300

    chore: reexport `wasi-preview1-component-adapter-provider` adapters
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@
 
 /// WASI preview1 -> preview2 component adapter for components of "command" type
 pub const WASI_PREVIEW1_COMMAND_COMPONENT_ADAPTER: &[u8] =
-    include_bytes!(env!("WASI_PREVIEW1_COMMAND_COMPONENT_ADAPTER"));
+    wasi_preview1_component_adapter_provider::WASI_SNAPSHOT_PREVIEW1_COMMAND_ADAPTER;
 
 /// WASI preview1 -> preview2 component adapter for components of "reactor" type
 pub const WASI_PREVIEW1_REACTOR_COMPONENT_ADAPTER: &[u8] =
-    include_bytes!(env!("WASI_PREVIEW1_REACTOR_COMPONENT_ADAPTER"));
+    wasi_preview1_component_adapter_provider::WASI_SNAPSHOT_PREVIEW1_REACTOR_ADAPTER;
